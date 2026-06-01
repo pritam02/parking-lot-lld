c0073e62d3a1a1297bdd83b19f45b6416c51c625
Pritam Pal <pritam@Pritams-MacBook-Air.local>
2026-06-01 15:21:12 +0530

    small change

--- src/main/tests/java/com/parkinglot/service/ParkingServiceTest.java
+++ src/main/tests/java/com/parkinglot/service/ParkingServiceTest.java
@@ -10,7 +10,6 @@ import com.parkinglot.model.Vehicle;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
